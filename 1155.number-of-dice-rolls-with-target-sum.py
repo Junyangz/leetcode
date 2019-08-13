0301714ff459b139115dbf98e5fae70cb82cc152
Junyangz <junyangz.iie@gmail.com>
2019-08-13 23:29:43 +0800

    Update 1155

--- 1155.number-of-dice-rolls-with-target-sum.py
+++ 1155.number-of-dice-rolls-with-target-sum.py
@@ -1,13 +1,26 @@
 class Solution:
-    def numRollsToTarget(self, n: int, m: int, x: int) -> int:
-        table = [[0]*(x+1) for i in range(n+1)]
+    memo = {}
+    def numRollsToTarget(self, d: int, f: int, target: int) -> int:
+        if (d, f, target) in self.memo:
+            return self.memo[d, f, target]
+        if d > target or target > d * f:
+            return 0
+        if d == 1:
+            return 1
+        self.memo[d, f, target] = sum(self.numRollsToTarget(d - 1, f, target - x) 
+                                      for x in range(1, f + 1)) % (10 ** 9 + 7)
+        
+        return self.memo[d, f, target]
 
-        for j in range(1,min(m+1, x+1)):
-            table[1][j] = 1
+#    def numRollsToTarget(self, n: int, m: int, x: int) -> int:
+        # table = [[0]*(x+1) for i in range(n+1)]
 
-        for i in range(2, n+1): 
-            for j in range(1, x+1): 
-                for k in range(1, min(m+1, j)): 
-                    table[i][j] += table[i-1][j-k] 
+        # for j in range(1,min(m+1, x+1)):
+        #     table[1][j] = 1
 
-        return table[-1][-1] % (10**9 + 7)
+        # for i in range(2, n+1): 
+        #     for j in range(1, x+1): 
+        #         for k in range(1, min(m+1, j)): 
+        #             table[i][j] += table[i-1][j-k] 
+
+        # return table[-1][-1] % (10**9 + 7)
